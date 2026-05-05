74686ce88214f084df9ca679ea60aa77e7713664
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2026-05-05 09:46:11 +0800

    Used useCallBack for addToCart

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+// components/ProductCard.tsx
+import React, { useContext, useCallback } from "react";
 import { type ProductCardProps, type Product } from "../types/types";
 
 import { useCartActions, useCart } from "../contexts/CartContextProvider";
@@ -10,13 +11,17 @@ export const ProductCard = React.memo(function ProductCard({ product }: ProductC
     // const cart = useCart();
     // console.log(cart);
 
+    // useCallback 
+
+    let onAddToCart = useCallback(() => { addToCart(product) }, [addToCart, product]);
+
     // AddToCart Button
     return (
         <div style={{ border: '1px solid black', padding: '1rem', margin: '1rem' }}>
             <h3>{product.name}</h3>
             <p>Price: ${product.price}</p>
             <p>Qty Available: {product.quantity}</p>
-            <button onClick={() => addToCart(product)}>
+            <button onClick={onAddToCart}>
                 Add to Cart
             </button>
         </div>
